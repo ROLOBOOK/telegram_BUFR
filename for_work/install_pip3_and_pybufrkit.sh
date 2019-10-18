36ca930a8e06e0a607fd01d995f909c6a721b8d4
ROLOBOOK <45481648+ROLOBOOK@users.noreply.github.com>
2019-10-18 14:16:56 +0300

    -y,  Automatic yes to prompts;

--- for_work/install_pip3_and_pybufrkit.sh
+++ for_work/install_pip3_and_pybufrkit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-sudo apt-get install mysql-server
-sudo apt-get install python3-pip
-sudo apt-get install python-dev default-libmysqlclient-dev #for mysqlclient
+sudo apt-get install -y mysql-server
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-dev default-libmysqlclient-dev #for mysqlclient
 pip3 install pybufrkit
 pip3 install mysqlclient
