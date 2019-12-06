01227a8f884a8f0abf53cf0a0c3216baf3abbf52
ROLOBOOK <45481648+ROLOBOOK@users.noreply.github.com>
2019-12-06 22:43:46 +0300

    install mySQLClient

---
+++ for_work/mySQLClient.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+pip3 install mysqlclient
+
+# sudo apt-get install -y python-dev default-libmysqlclient-dev #for mysqlclient
+# sudo apt-get install build-essential libssl-dev libffi-dev python-dev #for mysqlclient
+
+
