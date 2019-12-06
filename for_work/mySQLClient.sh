3e6864765c9617c08ca456afb59b505390c1cc1c
ROLOBOOK <45481648+ROLOBOOK@users.noreply.github.com>
2019-12-06 22:47:12 +0300

    update script MySQLclient

--- for_work/mySQLClient.sh
+++ for_work/mySQLClient.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
+
+# sudo apt-get install -y python-dev default-libmysqlclient-dev 
+# sudo apt-get install build-essential libssl-dev libffi-dev python-dev 
+
 pip3 install mysqlclient
 
-# sudo apt-get install -y python-dev default-libmysqlclient-dev #for mysqlclient
-# sudo apt-get install build-essential libssl-dev libffi-dev python-dev #for mysqlclient
+
 
 
