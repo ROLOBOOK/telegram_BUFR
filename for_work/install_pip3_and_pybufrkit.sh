e727f6bae4e42eacd296a36b27a20b7f38f81022
ROLOBOOK <45481648+ROLOBOOK@users.noreply.github.com>
2019-12-06 22:42:20 +0300

    del from script mysqlclient

--- for_work/install_pip3_and_pybufrkit.sh
+++ for_work/install_pip3_and_pybufrkit.sh
@@ -2,6 +2,9 @@
 
 sudo apt-get install -y mysql-server
 sudo apt-get install -y python3-pip
-sudo apt-get install -y python-dev default-libmysqlclient-dev #for mysqlclient
 pip3 install pybufrkit
-pip3 install mysqlclient
+
+#  sudo apt-get install -y python-dev default-libmysqlclient-dev #for mysqlclient
+#  sudo apt-get install build-essential libssl-dev libffi-dev python-dev #for mysqlclient
+
+#  pip3 install mysqlclient
