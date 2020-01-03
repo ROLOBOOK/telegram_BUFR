869806f048d23adc5845a7e28602d8a8fda424ef
ROLOBOOK <45481648+ROLOBOOK@users.noreply.github.com>
2020-01-03 19:59:53 +0300

    fix mistake string 9-10

--- for_work/setup.sh
+++ for_work/setup.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/bash
-
 sudo apt-get install -y mysql-server
 sudo apt-get install -y python3-pip
-
 pip3 install pybufrkit
-
-sudo apt-get install -y python-dev default-libmysqlclient-dev #for mysqlclient
-sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev #for mysqlclient
-
+sudo apt-get install -y python-dev default-libmysqlclient-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
 pip3 install mysqlclient
-
-
 sudo mysql < tsaoscript.sql
-
+cd ..
 python3 ../in_table_MYSQL_UGMS_and_Stations.py
 
 echo software installed and tested congratulations
