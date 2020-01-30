741bd86d27ef5dcee83118539eeb58789aa7c242
fol <rolobook@yandex.ru>
2020-01-30 16:32:09 +0300

    new base + index station in base

--- for_work/clear_base.sh
+++ for_work/clear_base.sh
@@ -6,3 +6,11 @@ else
 echo 'can not create base CAO'
 fi
 
+cd ..
+python3 add_new_station_in_base.py
+if [[ $? = 0 ]]
+then
+echo 'index_stations in base'
+else
+echo 'index_stations not in baseO'
+fi
