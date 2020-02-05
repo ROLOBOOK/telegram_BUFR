fb9d2d976e43af2591cb7dc26e64bf8a1842c7c5
fol <rolobook@yandex.ru>
2020-02-05 21:45:37 +0300

    fix proseccing

--- for_work/processing_telegram.py
+++ for_work/processing_telegram.py
@@ -24,6 +24,7 @@ def get_values_from_bufr(descriptor, key=0):
     return data[0] if data else None
 
 def decod_b(data):
+    '''декодирует байтовую строку из телеграммы'''
     try:
         return data.decode('cp1251')
     except:
@@ -31,6 +32,7 @@ def decod_b(data):
         return ''.join(data).replace(' ', '')
 
 def get_data_for_info_pusk(bufr_message, kye=0):
+    '''получаем декодированную телеграмму, на выходе списко значений для записи в базу данных '''
     index_station = '{:03d}{:03d}'.format(get_values_from_bufr("001001",kye),get_values_from_bufr("001002",kye))
     time_pusk = f'{get_values_from_bufr("004001",kye)}-{get_values_from_bufr("004002",kye)}-{get_values_from_bufr("004003",kye)} {get_values_from_bufr("004004",kye)}:{get_values_from_bufr("004005",kye)}:00'
     koordinat = f'{get_values_from_bufr("005001",kye)} {get_values_from_bufr("006001",kye)} {get_values_from_bufr("007030",kye)} {get_values_from_bufr("007031",kye)} {get_values_from_bufr("007007",kye)}'
@@ -79,72 +81,76 @@ except Exception as ex:
     log_mistake("ошибка получения списка телеграмм для: ", f'{ex}\n')
     print('Критическая ошибка, проверьте логи.')
     exit()
-print(f'для проверки загруженно {len(files)} телеграмм.')
 if not files:
     print('Не получены файлы для проверки')
     exit()
+print(f'для проверки загруженно {len(files)} телеграмм.')
 
 count_telegram = 0
 
-for file_name in files:
-    print(file_name)
+try:
+    conn = MySQLdb.connect(conect_bd.server, conect_bd.username,\
+                           conect_bd.password, conect_bd.bd, charset="utf8")
+    cursor = conn.cursor()
+
+
+    for file_name in files:
+        try:
+            decoder = Decoder()
+            with open(f'folder_with_telegram/{file_name}', 'rb') as ins: #
+                bufr_message = decoder.process(ins.read())
+        except Exception as ex:
+            log_mistake(file_name, f'не декодирован, {ex}\n')
+            try:
+                os.rename(f'folder_with_telegram/{file_name}', f'folder_with_telegram/file_with_mistakes/{file_name}')
+            except Exception as ex:
+                log_mistake(file_name, f'не декодирован и не перенесен в папку с ошибками, {ex}\n')
+                print('oy!')
+            continue
+        json_data = FlatJsonRenderer().render(bufr_message)
+        d = '{}-{:02d}-{:02d}'.format(json_data[1][-6], json_data[1][-5], json_data[1][-4])
+        t = '{:02d}:{:02d}:{:02d}'.format(json_data[1][-3], json_data[1][-2], json_data[1][-1] )
+        time_srok =  f'{d} {t}'
+
+        for kye in range(len(json_data[3][2])):
+            try:
+                data_in_cao_info_pusk = get_data_for_info_pusk(bufr_message,kye)
+                text_info = decod_b(json_data[3][2][kye][-1]) if type(json_data[3][2][kye][-1]) == bytes else None
+                data_in_cao_info_pusk.append(time_srok)
+                data_in_cao_info_pusk.append(text_info)
+            except Exception as ex:
+                log_mistake(file_name, ex)
+            if not data_in_cao_info_pusk:
+                print(file_name, 'problems')
+
+                cursor.execute('''
+	            INSERT IGNORE INTO cao2.info_pusk
+	            (index_station, time_pusk, koordinat, oborudovanie, oblachnost, sdvig_vetra, metod_opredeleniy_visoti, 
+	             po_versia, s_n_zonda, algoritm_popravok_izmereniy_vlagnosti, nesyshay_chastota, datchik_davleniy, 
+	             datchik_temperatur, datchik_vlagnosti, nomer_nabludenia, nomer_zondirovania, fio_nabludateliy, 
+	             nazemnaiy_sistema_priema_signalov, visota, visota_anteni, popravka_azimut, popravka_ygl, radio_ykritie, configur_zonda, 
+	             configur_podveski_zonda, proizvoditel_obolochki, tip_obolochki, massa_obolochki, gaz_dly_napolnenia, kolichestbo_gaza, dlina_podvesa, 
+	             prichina_prikrashenia, time_srok, text_info)
+	            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',data_in_cao_info_pusk)
+                conn.commit()
+
+            count_telegram+=1
+            if  time.time() - begintime > minut:
+                minut+=random.uniform(50,130)
+                t = time.time() - begintime
+                print('Сначала проверки прошло {:02d}:{:02d}:{:02d}, обработано {} телеграмм'.format(int(t//3600%24), int(t//60%60), int(t%60), count_telegram))
     try:
-        decoder = Decoder()
-        with open(f'folder_with_telegram/{file_name}', 'rb') as ins: #
-            bufr_message = decoder.process(ins.read())
-    except Exception as ex:
-        log_mistake(file_name, f'не декодирован, {ex}\n')
-        try:
-            os.rename(f'folder_with_telegram/{file_name}', f'folder_with_telegram/file_with_mistakes/{file_name}')
-        except Exception as ex:
-            print('oy!')
-        continue
-    json_data = FlatJsonRenderer().render(bufr_message)
-    d = '{}-{:02d}-{:02d}'.format(json_data[1][-6], json_data[1][-5], json_data[1][-4])
-    t = '{:02d}:{:02d}:{:02d}'.format(json_data[1][-3], json_data[1][-2], json_data[1][-1] )
-    time_srok =  f'{d} {t}'
-
-
-    for kye in range(len(json_data[3][2])):
-        try:
-            data_in_cao_info_pusk = get_data_for_info_pusk(bufr_message,kye)
-            text_info = decod_b(json_data[3][2][kye][-1]) if type(json_data[3][2][kye][-1]) == bytes else None
-            data_in_cao_info_pusk.append(time_srok)
-            data_in_cao_info_pusk.append(text_info)
-        except Exception as ex:
-            log_mistake(file_name, ex)
-        if not data_in_cao_info_pusk:
-            print(file_name, 'problems')
-        try:
-            conn = MySQLdb.connect(conect_bd.server, conect_bd.username,\
-                               conect_bd.password, conect_bd.bd, charset="utf8")
-            cursor = conn.cursor()
-
-            cursor.execute('''
-            INSERT IGNORE INTO cao2.info_pusk
-            (index_station, time_pusk, koordinat, oborudovanie, oblachnost, sdvig_vetra, metod_opredeleniy_visoti, 
-             po_versia, s_n_zonda, algoritm_popravok_izmereniy_vlagnosti, nesyshay_chastota, datchik_davleniy, 
-             datchik_temperatur, datchik_vlagnosti, nomer_nabludenia, nomer_zondirovania, fio_nabludateliy, 
-             nazemnaiy_sistema_priema_signalov, visota, visota_anteni, popravka_azimut, popravka_ygl, radio_ykritie, configur_zonda, 
-             configur_podveski_zonda, proizvoditel_obolochki, tip_obolochki, massa_obolochki, gaz_dly_napolnenia, kolichestbo_gaza, dlina_podvesa, 
-             prichina_prikrashenia, time_srok, text_info)
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',data_in_cao_info_pusk)
-            conn.commit()
-        except Exception as ex:
-            log_mistake(file_name, ex)
-            print(file_name, ex)
-        finally:
-            conn.close()
-        count_telegram+=1
-        if  time.time() - begintime > minut:
-            minut+=random.uniform(50,130)
-            t = time.time() - begintime
-            print('Сначала проверки прошло {:02d}:{:02d}:{:02d}, обработано {} телеграмм'.format(int(t//3600%24), int(t//60%60), int(t%60), count_telegram))
-
-    try:
-        os.rename(f'folder_with_telegram/{file_name}', f'folder_with_telegram/cheking_telegram/{file_name}')
+        os.rename(f'folder_with_telegram/{file_name}', f'folder_with_telegram/cheking_telegram{file_name}')
     except Exception as ex:
         log_mistake(file_name, ex)
+
+
+except Exception as ex:
+    log_mistake(file_name, ex)
+    print(file_name, ex)
+finally:
+    conn.close()
+
 
 t = time.time() - begintime
 print('Проверка закончена за {:02d}:{:02d}:{:02d}'.format(int(t//3600%24), int(t//60%60), int(t%60)))
